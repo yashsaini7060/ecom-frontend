6ff7b5c05387a8ab8ccee9598b35167f08883a59
yashsaini7060 <yk18052000@gmail.com>
2023-02-14 23:57:40 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Navbar from './components/navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Navbar />
 );
 
